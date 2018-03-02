669d95f4b99f02b51b0f320d429f80bbd36793d0
XXX <atomxxx@gmail.com>
2018-03-02 21:11:33 +0700

    Branch2

---
+++ src/hello/XxX.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class XxX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Check mate check mate!!!");
+		System.out.println(" Test  lan nua");
+		System.out.println("Test local");
+		System.out.println("xxx");
+		System.out.println("Check met vd");
+	}
+
+}
---
+++ src/hello/hello.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/** Bo nghich ti thoi*/
+	
+		
+			   System.out.println("Hello World. I'm Dzung , I'm here to conquer the world");  
+			    } 
+}
